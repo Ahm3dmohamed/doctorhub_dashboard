4b32cd0e7c08c0145865b25e223191b870588c84
Ahm3dmohamed <you@example.com>
2026-08-08 22:26:07 +0300

    deploy: update live Flutter Web build with 404.html and .nojekyll for GitHub Pages

--- lib/features/reports/domain/repositories/report_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/errors/failures.dart';
-import '../entities/report_entity.dart';
-
-abstract class ReportRepository {
-  Future<Either<Failure, ReportSummaryEntity>> getReportSummary({String? period});
-}
